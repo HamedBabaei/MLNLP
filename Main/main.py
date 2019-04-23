f9bccacf92aef0bf75f8def0b572b69180b77f03
Erfan Mohammadzadeh <e.mohammadzadeh78@gmail.com>
2019-04-23 14:09:11 +0430

    main.py script updated!

--- Main/main.py
+++ Main/main.py
@@ -1,9 +1,44 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+import json
+import codecs
+import os
+#import data_visualization as dv
+
 class Pan:
+    english_stopwords = set(nltk.corpus.stopwords.words('english'))
+        
     #preperacessing
-    def Tokenizer(self , text, **args):
-        pass
-    def Pos_tagger(self, text, **args):
-        pass
+    def Sentence_tokenizer(self , text, **args):
+        if len(args) < 1:
+            args["tokenizer"] = "nltk"
+        if args["tokenizer"] == "nltk":
+            self.sentences_tokenize = nltk.sent_tokenize(text)
+        return self.sentences_tokenize
+
+    def Word_tokenizer(self, text, **args):
+        if len(args) < 1:
+            args["tokenizer"] = "nltk"
+        if args["tokenizer"] == "nltk":
+            self.words_tokenize = nltk.word_tokenize(text)
+        else:
+            pass
+        return self.words_tokenize
+
+    def Stop_words_filtering(self, text):
+        tokens = word_tokenize(text)
+        self.contenet_tokens_without_stopwords = [token for token in tokens if token.lower() not in self.english_stopwords]
+        return self.contenet_tokens_without_stopwords 
+
+    def Pos_tagger(self, tokens, **args):
+        if len(args) < 1:
+            args["pos_tagger"] = "nltk"
+        if args["pos_tagger"] == "nltk":
+            self.token_pos_tag = pos_tag(tokens)
+        return self.token_pos_tag
+            
     def Terms_counter(self, text, **args):
         pass
 
@@ -17,21 +52,122 @@ class Pan:
     def Hist(self, **args):
         pass
 
-    #machine learning
-    #performance
-    def Precision(self, perdiction, truth):
-        pass
-    def Recall(self, perdiction, truth):
-        pass
-    def F1(self, perdicition, truth):
-        pass
-    def Accuracy(self, perdicition , truth):
-        pass
-    def C_at_1(self, perdicition, truth):
+    #machine learning & natural language processing
+    def Tf_idf(self):
         pass
 
+    def Bag_of_words(self, **text):
+        pass  
+
+    #interpret NLP
+    def Stop_words(self):
+        #print(self.english_stopwords)
+        return self.english_stopwords
+
+    def Precision(self):
+        # TP, TN, FP, FN = self.Tp_tn_fp_fn(self)
+        self.precision = self.TP / self.TP + self.FP
+
+        return self.precision
+    
+    def Recall(self):
+        # TP, TN, FP, FN = self.Tp_tn_fp_fn(self)
+        # self.recall = TP / TP + FN
+        self.recall = self.TP / self.TP + self.FN
+
+        return self.recall
+    
+    def F1(self):
+        # TP, TN, FP, FN = self.Tp_tn_fp_fn(self)
+        f1_score = 2 * (self.recall * self.precision) / (self.recall + self.precision)
+
+        return f1_score
+    
+    def Accuracy(self):
+        # TP, TN, FP, FN = self.Tp_tn_fp_fn(self)
+        # self.accuracy = TP + TN / TP + FP + FN + TN
+        self.accuracy = self.TP + self.TN / self.TP + self.FP + self.FN + self.TN
+
+    def C_at_1(self):
+        pass
+    
 class Cross_Domain_Authorship_Attribution(Pan):
-    pass
+    merge_candidates = False
+    all_candidates_txts = {}
+    dataset_root_dir = ""
+    #dataset path must be provided
+
+    def Read_json_file(self, json_file_name):
+        with codecs.open(json_file_name, 'r', encoding= 'utf-8') as json_read:
+            self.json_context = json.load(json_read)
+        return self.json_context
+    
+    #arg[0] for determining two or more unknown  and args[1] 
+    def Normal_context(self, **args):
+        self.truth_problem = {}
+        if args['whole_documents'] == 'on':
+             for key, values in self.json_context.items():
+                 self.truth_problem[key] = values['truth']
+                 #just change the return code and make it with one line code
+             return self.truth_problem
+        elif args['whole_documents'] == 'off':  
+            self.truth_problem[args['target_problem']] = self.json_context[args['target_problem']]['truth']
+            return self.truth_problem
+        else:
+            # raise
+            pass
+    
+        #performance
+    def Confusion_matrix(self, prediction):
+        self.TP, self.TN, self.FP, self.FN = 0, 0, 0, 0
+        f = open('confusion.txt','w')
+        for problem_id, problem_value in self.truth_problem.items():
+            for truth_id, truth_value in problem_value.items():
+                for i in range(10):
+                    if i == prediction[problem_id][truth_id] and prediction[problem_id][truth_id] == problem_value[truth_id]:
+                        self.TP += 1
+                    elif i == prediction[problem_id][truth_id] and prediction[problem_id][truth_id] != problem_value[truth_id]:
+                        self.FP += 1
+                    elif i != prediction[problem_id][truth_id] and i == problem_value[truth_id]:
+                        self.FN += 1
+                    elif i != prediction[problem_id][truth_id] and i != problem_value[truth_id]:
+                        self.TN += 1
+                    f.write('{} {} {} {}'.format(self.TP, self.TN, self.FP, self.FN))
+                    f.write("\n")
+                f.write('\n')
+                # print(self.TP, self.TN, self.FP, self.FN)
+        f.close()
+        #print(self.TP, self.TN, self.FP, self.FN)
+
+
+    def Set_dataset_dir(self , path):
+        self.dataset_root_dir = path
+
+
+    def Read_collection_info(self):
+        with codecs.open(os.path.join(self.dataset_root_dir , "collection-info.json") , 'r' , encoding="utf-8") as f:
+                return json.load(f)
+    
+    def Read_problems(self):
+        problems = self.Read_collection_info()  
+        for problem in problems:
+            print("working on problem : " , problem['problem-name'])
+            with codecs.open(os.path.join(self.dataset_root_dir , problem['problem-name'], 'problem-info.json') ,'r' , encoding='utf-8') as j:
+                problem_info = json.load(j)
+            candidates = [candidate['author-name'] for candidate in problem_info['candidate-authors']]
+            candidates_txts = {}
+            for candidate in candidates:
+                txt = []
+                for txt_name in os.listdir(os.path.join(self.dataset_root_dir , problem['problem-name'] , candidate)):
+                    txt_path = os.path.join(os.path.join(self.dataset_root_dir , problem['problem-name'] , candidate , txt_name))
+                    with codecs.open(txt_path , 'r' , encoding='utf-8') as f:
+                        txt.append(f.read())
+                if self.merge_candidates:
+                    candidates_txts[candidate] = ' '.join(txt)
+                else:
+                    candidates_txts[candidate] = txt
+                    #yield candidates_txts
+            self.all_candidates_txts[problem['problem-name']] = candidates_txts
     
 class Style_Change_Detection(Pan):
     pass
@@ -42,6 +178,13 @@ class Celebrity_Profiling(Pan):
 class Bots_And_Gender_Profiling(Pan):
     pass
 
+
 def main():
-    pass
+    cross_domain_task = Cross_Domain_Authorship_Attribution()
+    prediction_dic = cross_domain_task.Read_json_file("all_truth.json")
+    prediction_normal = cross_domain_task.Normal_context(whole_documents = "on")
+    cross_domain_task.Read_json_file("all_truth.json")
+    cross_domain_task.Normal_context(whole_documents = "on")
+    cross_domain_task.Confusion_matrix(prediction_normal)
+
 if __name__ == "__main__": main()
